0ce77aee740d310f6bec67b859de3183a5032c6b
rjulia23 <47512918+rjulia23@users.noreply.github.com>
2019-02-24 01:04:55 -0800

    updated moving shapes

--- moving-shapes/js/index.js
+++ moving-shapes/js/index.js
@@ -5,28 +5,20 @@ var veryfast = 10;
 var docHeight = $(document).height();
 var docWidth = $(document).width();
 
-
-//$(".circle").offset({top:500+currentMousePos.y/amount,left:500+currentMousePos.x/amount});
-//$(".square").offset({top:300-currentMousePos.y/amount*3,left:1700-currentMousePos.x/amount*3});
 $(document).mousemove(function(event) {
   var docHeight = $(document).height();
   var docWidth = $(document).width();
   var x = event.pageX;
   var y = event.pageY;
-  
+   
   $("#shape-1").offset({top:(docHeight*.05)+y/slow,left:(docWidth*.03)+x/slow});
   $("#shape-2").offset({top:(docHeight*.50)-y/slow,left:(docWidth*.75)-x/slow});
-  $("#shape-3").offset({top:(docHeight*.20)+y/medium,left:(docWidth*.80)-x/medium});
-  /*
-  $("#shape-3").offset({top:(docHeight*top)+y/medium,left:(docWidth*left)-x/medium});
-  
-  */
-  
-
-   // $("#shape-1").offset({top:($(document).height()*.05)+y/slow,left:($(document).width()*.03)+x/slow});
-
- 
-  //  $(".square").offset({top:300-currentMousePos.y/amount*3,left:1700-currentMousePos.x/amount*3});
-  //  $(".triangle-outline").offset({top:100+currentMousePos.y/amount/3,left:100-currentMousePos.x/amount/2});
-   // $(".triangle").offset({top:200-currentMousePos.y/amount/2,left:100+currentMousePos.x/amount/3});
+  $("#shape-3").offset({top:(docHeight*.20)+y/medium,left:(docWidth*.85)-x/medium});
+  $("#shape-4").offset({top:(docHeight*.40)+y/veryfast,left:(docWidth*.05)+x/medium});
+  $("#shape-5").offset({top:(docHeight*.10)+y/fast,left:(docWidth*.20)-x/medium});
+  $("#shape-6").offset({top:(docHeight*.85)-y/fast,left:(docWidth*.90)-x/fast});
+  $("#shape-7").offset({top:(docHeight*.75)-y/medium,left:(docWidth*.20)-x/medium});
+  $("#shape-8").offset({top:-20-y/medium,left:(docWidth*.6)+x/medium});
+  $("#shape-9").offset({top:(docHeight*.5)-y/medium,left:(docWidth*.3)+x/medium});
+  $("#shape-10").offset({top:(docHeight*.90)-y/medium,left:(docWidth*.65)-x/veryfast});
 });
